03a66cdb077696b6ee834c506fa18f9212feed6f
will00 <wtony1239271@gmail.com>
2018-09-13 11:53:56 +0800

    前導頁調整2

--- G4_SASS/index.php
+++ G4_SASS/index.php
@@ -4,7 +4,7 @@
 <head>
         <meta charset="UTF-8">
         <title>Document</title>
-        <link rel="stylesheet" type="text/css" href="css/svg-text.css">
+        <link rel="stylesheet" type="text/css" href="csss/svg-text.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 </head>
 
